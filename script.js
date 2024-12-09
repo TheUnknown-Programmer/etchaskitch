0cdd00c50d1a8dcd1bf5106f73db8c3034066830
Jonathan <f.jonathan9700@gmail.com>
2024-12-09 11:54:06 -0500

    16X16 grid of square divs

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-const container = document.getElementById('container')
+const gridContainer = document.getElementById('grid-container');
 
-for (let i = 1; i <= 5; i++) {
-  // Create a new div element
-  const newDiv = document.createElement('div');
-
-  // Add class and content to the div
-  newDiv.className = 'dynamic-div';
-  newDiv.textContent = `Div ${i}`;
-
-  // Append the new div to the container
-  container.appendChild(newDiv);
+for (let i = 0; i < 16 * 16; i++) {
+  const gridItem = document.createElement('div');
+  gridItem.classList.add('grid-item');
+  gridContainer.appendChild(gridItem);
 }
