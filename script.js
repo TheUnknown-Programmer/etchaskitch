fc5f906ca2ba0d99004282971b986dab1616a939
Jonathan <f.jonathan9700@gmail.com>
2024-12-10 02:05:17 -0500

    hover effect

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ const gridContainer = document.getElementById('grid-container');
 for (let i = 0; i < 16 * 16; i++) {
   const gridItem = document.createElement('div');
   gridItem.classList.add('grid-item');
+
+  gridItem.addEventListener("mouseover", function() {
+    this.style.backgroundColor = "red";
+  });
+  
   gridContainer.appendChild(gridItem);
+  
 }
+
+
